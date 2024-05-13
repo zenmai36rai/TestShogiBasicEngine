47feeef78605d8d84a99eed7312c8bf63e30fe49
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2024-05-13 16:54:52 +0900

    エンジンテスト版試作

---
+++ ShogiBasicEngine/ShogiBasicEngine/Program.vb
@@ -0,0 +1,76 @@
+Imports System
+
+Module Program
+    Const ENGINE_NAME = "ShogiBasicEngine"
+    Const AUTHOR_NAME = "Kyosuke Miyazawa"
+    Class USI
+        Const ID_NAME = 0
+        Const ID_AUTHOR = 1
+        Const ID_NB = 2
+        Public USI()
+        Public Function UsiLoop() As Integer
+            Dim cmd As String = Console.ReadLine()
+            Dim cmds = Split(cmd)
+            While (cmds(0) <> "")
+                If (cmds(0).Equals("quit")) Then
+                    Return 0
+
+                ElseIf (cmds(0).Equals("usi")) Then
+                    SendId(ID_NAME)
+                    SendId(ID_AUTHOR)
+                    SendUsiok()
+
+                ElseIf (cmds(0).Equals("isready")) Then
+                    SendReadyOk()
+
+                ElseIf (cmds(0).Equals("usinewgame")) Then
+                    cmd = Console.ReadLine()
+                    cmds = Split(cmd)
+                    Continue While
+
+                ElseIf (cmds(0).Equals("position")) Then
+                    cmd = Console.ReadLine()
+                    cmds = Split(cmd)
+                    Continue While
+
+                ElseIf (cmds(0).Equals("go")) Then
+                    SendBestmove()
+
+                Else
+                    Console.WriteLine("invalid comannd:")
+                End If
+                cmd = Console.ReadLine()
+                cmds = Split(cmd)
+            End While
+            Return 0
+
+        End Function
+
+        Private ID = {ENGINE_NAME, AUTHOR_NAME, ""}
+        Private Function SendId(ByVal id As String)
+            If (id = ID_NAME) Then
+                Console.WriteLine("id name " + ENGINE_NAME)
+            ElseIf (id = ID_AUTHOR) Then
+                Console.WriteLine("id author " + AUTHOR_NAME)
+            Else
+                Return 0
+            End If
+        End Function
+        Private Function SendUsiok()
+            Console.WriteLine("usiok")
+            Return 0
+        End Function
+        Private Function SendReadyOk()
+            Console.WriteLine("readyok")
+            Return 0
+        End Function
+        Private Function SendBestmove()
+            Console.WriteLine("bestmove resign")
+            Return 0
+        End Function
+    End Class
+    Sub Main(args As String())
+        Dim usi As USI = New USI()
+        usi.UsiLoop()
+    End Sub
+End Module
